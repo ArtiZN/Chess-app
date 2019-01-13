c0ef0b01b8d58740ebfcf5c5e47072a21576cdd2
SonyaMarmeladova <7exalted7@gmail.com>
2019-01-13 22:39:02 +0200

    allow only valid moves

--- src/app/shared/components/chessground/chessground.component.ts
+++ src/app/shared/components/chessground/chessground.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
 
 import { Chessground } from 'chessground';
+import * as Chess from 'chess.js';
 
 @Component({
   selector: 'app-chessground',
@@ -16,6 +17,45 @@ export class ChessgroundComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const ground = Chessground(this.chessBoard.nativeElement);
+    const chess = new Chess();
+    const ground = Chessground(this.chessBoard.nativeElement, {
+      movable: {
+        color: 'white',
+        free: false,
+        dests: this.toDests(chess)
+      }
+    });
+
+    ground.set({
+      movable: { events: { after: this.playOtherSide(ground, chess) } }
+    });
+  }
+
+  toDests(chess: any) {
+    const dests = {};
+    chess.SQUARES.forEach(s => {
+      const ms = chess.moves({square: s, verbose: true});
+      if (ms.length) {
+        dests[s] = ms.map(m => m.to);
+      }
+    });
+    return dests;
+  }
+
+  playOtherSide(cg, chess) {
+    return (orig, dest) => {
+      chess.move({from: orig, to: dest});
+      cg.set({
+        turnColor: this.toColor(chess),
+        movable: {
+          color: this.toColor(chess),
+          dests: this.toDests(chess)
+        }
+      });
+    };
+  }
+
+  toColor(chess: any) {
+    return (chess.turn() === 'w') ? 'white' : 'black';
   }
 }
