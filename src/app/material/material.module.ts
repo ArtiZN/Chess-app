745d74a3fdfbc9f38fe5a151121f807335b405b7
SonyaMarmeladova <7exalted7@gmail.com>
2019-01-15 14:14:44 +0200

    apply some styles to table

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   exports: [
-    MatMenuModule
+    MatMenuModule,
+    MatTableModule
   ],
   declarations: []
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,22 @@
+import { MaterialModule } from './../material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ChessgroundComponent } from './components/chessground/chessground.component';
 import { MovesTableComponent } from './components/moves-table/moves-table.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
   exports: [
-    ChessgroundComponent
+    ChessgroundComponent,
+    MovesTableComponent
   ],
-  declarations: [ChessgroundComponent, MovesTableComponent]
+  declarations: [
+    ChessgroundComponent,
+    MovesTableComponent
+  ]
 })
 export class SharedModule { }
