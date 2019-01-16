0666493e07b77341f3cf7289eac01e296464f830
SonyaMarmeladova <7exalted7@gmail.com>
2019-01-16 20:07:41 +0200

    add message service

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const express = require('express');
+const socketIO = require('socket.io');
 const http = require('http');
 const path = require('path');
 
 let app = express();
 let server = http.createServer(app);
+let io = socketIO(server);
+
+io.on('connection', socket => {
+  console.log('new user connected');
+
+  socket.on('disconnect', () => {
+    console.log('use is disconnected');
+  });
+});
 
 app.use(express.static(path.join(__dirname, './public')));
 
