4f8cbba2fe8adc01a4b5458db15dd7cc8d42a69c
SonyaMarmeladova <7exalted7@gmail.com>
2019-01-16 18:55:16 +0200

    add footer to chat component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
@@ -6,8 +7,13 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
+  imports: [
+    BrowserAnimationsModule
+  ],
   exports: [
     MatMenuModule,
     MatTableModule,
@@ -15,7 +21,9 @@ import { MatDividerModule } from '@angular/material/divider';
     MatCardModule,
     MatButtonModule,
     MatListModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }
