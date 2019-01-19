3d545469320eae4a9317ec9f14f1aebb15d8e599
SonyaMarmeladova <7exalted7@gmail.com>
2019-01-19 18:15:22 +0200

    change tsconfig

--- src/app/core/services/websocket/websocket.service.ts
+++ src/app/core/services/websocket/websocket.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject, Observable, Subscriber } from 'rxjs';
-import { environment } from '@environment/environment';
+import { environment } from '@environment/enviroment';
 
 import * as io from 'socket.io-client';
 
