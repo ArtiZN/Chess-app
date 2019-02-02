d5b67f2e953930f19dcfda6795098e912e85bae2
SonyaMarmeladova <7exalted7@gmail.com>
2019-02-02 16:56:48 +0200

    add auth service

--- src/app/core/mock-backend/interceptors/error.interceptor.ts
+++ src/app/core/mock-backend/interceptors/error.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { AuthenticationService } from '../services/auth-service';
+import { AuthenticationService } from '../services/auth.service';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
