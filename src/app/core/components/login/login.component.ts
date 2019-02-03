8c2fa5821ea2f1ce051743f022165a28e5697fd3
SonyaMarmeladova <7exalted7@gmail.com>
2019-02-03 14:58:40 +0200

    create login.html scratch

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { LogInFormGroup } from './../../models/login.form-group';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form: LogInFormGroup;
+  errorMessage: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.form = new LogInFormGroup();
+    document.body.style.backgroundColor = '#f6f6f6';
   }
 
+  login(form) {
+    // this.authService.login(form.value)
+    //   .subscribe(response => {
+    //     if(response.hasOwnProperty("Error")){
+    //       this.errorMessage = response["Error"];
+    //       console.log(this.errorMessage)
+    //     }
+    //     else {
+    //       this.userService.updateUser(response);
+    //       this.router.navigateByUrl("/");
+    //     }
+    //   })
+  }
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,8 @@
-import { MaterialModule } from './../material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from './../material/material.module';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
@@ -8,6 +10,8 @@ import { LoginComponent } from './components/login/login.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
     MaterialModule
   ],
   exports: [
