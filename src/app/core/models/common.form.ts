2f3e3ee83e3da1605fcedea69ed63535bb3ec93d
SonyaMarmeladova <7exalted7@gmail.com>
2019-02-03 14:38:31 +0200

    add common form

---
+++ src/app/core/models/common.form.ts
@@ -0,0 +1,40 @@
+import { FormControl } from '@angular/forms';
+
+export class CommonFormControl extends FormControl {
+
+  constructor(
+    private label: string,
+    private _type: string,
+    private modelProperty: string,
+    value: any,
+    validator: any) {
+      super(value, validator);
+  }
+
+  getValidationMessages(): string[] {
+    const messages: string[] = [];
+    if (this.errors) {
+      for (const errorName of Object.keys(this.errors)) {
+        switch (errorName) {
+          case 'required':
+            messages.push(`You must enter a ${this.label}`);
+            break;
+          case 'minlength':
+            messages.push(`A ${this.label} must be at least ${this.errors['minlength'].requiredLength} characters`);
+            break;
+          case 'maxlength':
+            messages.push(`A ${this.label} must be no more than ${this.errors['maxlength'].requiredLength} characters`);
+            break;
+          case 'pattern':
+            messages.push(`The ${this.label} contains illegal characters`);
+            break;
+        }
+      }
+    }
+    return messages;
+  }
+
+  get type(): string {
+    return this._type;
+  }
+}
