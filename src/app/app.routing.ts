ddd5f186259f7c005f4cba88eeaa765a8422798b
SonyaMarmeladova <7exalted7@gmail.com>
2019-04-06 17:56:56 +0300

    use auth guard

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,4 @@
+import { AuthGuardService } from './guards/auth-guard.service';
 import { ChessGameComponent } from '@core/components/chess-game/chess-game.component';
 import { CheakersGameComponent } from '@core/components/cheakers-game/cheakers-game.component';
 import { NotFoundComponent } from '@core/components/not-found/not-found.component';
@@ -11,9 +12,9 @@ const routes: Routes = [
   }, {
     path: 'login', component: LoginComponent
   }, {
-    path: 'cheakers', component: CheakersGameComponent
+    path: 'cheakers', component: CheakersGameComponent, canActivate: [AuthGuardService]
   }, {
-    path: 'chess', component: ChessGameComponent
+    path: 'chess', component: ChessGameComponent, canActivate: [AuthGuardService]
   }, {
     path: '404', component: NotFoundComponent
   }, {
