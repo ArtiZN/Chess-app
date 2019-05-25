a81b60cfbfd0f3410df606d803a6d1ce715fdfb7
SonyaMarmeladova <7exalted7@gmail.com>
2019-05-25 19:17:15 +0300

    online list structure

--- src/app/shared/components/friend-list/friend-list.component.ts
+++ src/app/shared/components/friend-list/friend-list.component.ts
@@ -14,15 +14,31 @@ export class FriendListComponent implements OnInit {
     status: 'online',
     name: 'friend 2'
   }, {
-    status: 'offline',
+    status: 'online',
     name: 'friend 3'
   }, {
     status: 'offline',
     name: 'friend 4'
+  }, {
+    status: 'online',
+    name: 'friend 5'
+  }, {
+    status: 'offline',
+    name: 'friend 6'
+  }, {
+    status: 'offline',
+    name: 'friend 7'
   }];
+
+  onlineCollapsed = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onlineClick() {
+    console.log('online click');
+    this.onlineCollapsed = !this.onlineCollapsed;
   }
 }
