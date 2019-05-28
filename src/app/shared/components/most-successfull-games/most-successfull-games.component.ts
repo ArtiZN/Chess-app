069200e82e51477e6c777fc53518f8eb1ce7f898
SonyaMarmeladova <7exalted7@gmail.com>
2019-05-28 22:24:06 +0300

    refactoring

--- src/app/shared/components/most-successfull-games/most-successfull-games.component.ts
+++ src/app/shared/components/most-successfull-games/most-successfull-games.component.ts
@@ -7,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MostSuccessfullGamesComponent implements OnInit {
 
+  games = [{
+    src: './assets/images/games/ruy-lopez.png',
+    title: '1) Ruy Lopez opening - white'
+  }, {
+    src: './assets/images/games/scotch-game.png',
+    title: '2) Scotch-game - white'
+  }, {
+    src: './assets/images/games/grunfeld-defence.png',
+    title: '3) Grunfeld defence - black'
+  }];
+
   constructor() { }
 
   ngOnInit() {
